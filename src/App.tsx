c02365cd61016d51a8a3d0240701ce9826cdf9f1
Marcus Vinícius Santana Lourenço <94205248+mav273@users.noreply.github.com>
2023-05-26 19:55:15 +0000

    d

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,9 @@ function App() {
         setToken(response.data);
         setError(null);
       })
-      .catch(setError);
+      .catch((e) => {
+        console.log(e)
+      });
   
     })
   }
